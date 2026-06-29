33f6d4d913dfb5190e2d90894a02e67b8fb84850
HarelValfish <harelvalfish@gmail.com>
2026-06-29 09:48:32 +0300

    Refactor ACM certificate configuration and remove unused Route 53 outputs

--- infra/terraform/dns.tf
+++ infra/terraform/dns.tf
@@ -1,27 +1,14 @@
 resource "aws_acm_certificate" "main" {
   domain_name               = "harelvalfish.dev"
-  subject_alternative_names = ["*.${"harelvalfish.dev"}"]
+  subject_alternative_names = ["*.harelvalfish.dev"]
   validation_method         = "DNS"
   lifecycle { create_before_destroy = true }
 }
 
-resource "aws_route53_zone" "main" {
-  name = "harelvalfish.dev"
+output "acm_validation_cname_name" {
+  value = tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_name
 }
 
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => dvo
-  }
-  zone_id         = aws_route53_zone.main.zone_id
-  name            = each.value.resource_record_name
-  type            = each.value.resource_record_type
-  records         = [each.value.resource_record_value]
-  ttl             = 60
-  allow_overwrite = true
-}
-
-resource "aws_acm_certificate_validation" "main" {
-  certificate_arn         = aws_acm_certificate.main.arn
-  validation_record_fqdns = [for r in aws_route53_record.cert_validation : r.fqdn]
+output "acm_validation_cname_value" {
+  value = tolist(aws_acm_certificate.main.domain_validation_options)[0].resource_record_value
 }
